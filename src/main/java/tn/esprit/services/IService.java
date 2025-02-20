74ea8c74857533dbbfa280a7a20538c8f97dc1b9
Asma Drissi <asmadrissi@yahoo.com>
2025-02-20 23:20:41 +0100

    CRUD and scene builder

---
+++ src/main/java/tn/esprit/services/IService.java
@@ -0,0 +1,14 @@
+package tn.esprit.services;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IService<T> {
+    void add(T t) throws SQLException;
+    void addP(T t) throws SQLException;
+    void delete(int id) throws SQLException;
+    void update(int id,T t) throws SQLException;
+    List<T> returnList() throws SQLException;
+
+
+}
